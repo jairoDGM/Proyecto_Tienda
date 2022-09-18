320b0538ae7f50ea0d452800e2df35070247276f
jairoDGM <jairo.danielgm@gmail.com>
2022-09-17 19:32:44 -0600

    Subida Avance actual componentes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CarritoCompraComponent } from './carrito-compra/carrito-compra.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EntregaTrackerComponent } from './entrega-tracker/entrega-tracker.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MainToolbarComponent } from './main-toolbar/main-toolbar.component';
+import { PagoFormularioComponent } from './pago-formulario/pago-formulario.component';
+import { ProductosCatalogoComponent } from './productos-catalogo/productos-catalogo.component';
+import { RegistroComponent } from './registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"registro",component:RegistroComponent},
+  {path:"productos-catalogo",component:ProductosCatalogoComponent},
+  {path:"pago-formulario",component:PagoFormularioComponent},
+  {path:"main-toolbar",component:MainToolbarComponent},
+  {path:"inicio",component:InicioComponent},
+  {path:"entrega-tracker",component:EntregaTrackerComponent},
+  {path:"dashboard",component:DashboardComponent},
+  {path:"carrito-compra",component:CarritoCompraComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
