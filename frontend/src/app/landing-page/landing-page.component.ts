51cf39506a6bdb3c05f807bbfae549af83aa5493
jairoDGM <jairo.danielgm@gmail.com>
2022-09-18 16:02:24 -0600

    creacion componente landin page

---
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+export class LandingPageComponent implements OnInit {
+
+  constructor(private route:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  mover1(){
+    this.route.navigateByUrl("/carrito-compra");
+  }
+  mover2(){
+    this.route.navigateByUrl("/dashboard");
+  }
+  mover3(){
+    this.route.navigateByUrl("/entrega-tracker");
+  }
+  mover4(){
+    this.route.navigateByUrl("/inicio");
+  }
+  mover5(){
+    this.route.navigateByUrl("/main-toolbar");
+  }
+  mover6(){
+    this.route.navigateByUrl("/pago-formulario");
+  }
+  mover7(){
+    this.route.navigateByUrl("/productos-catalogo");
+  }
+  mover8(){
+    this.route.navigateByUrl("/registro");
+  }
+}
