5ff8714be24a07ab9e8cc93d036d80b27420c24b
Abel <abel.alvarez@galileo.edu>
2022-09-23 23:28:59 -0600

    Carrito V1 y Cambios fondos Productos y Agrego Fondos

--- frontend/src/app/carrito-compra/carrito-compra.component.ts
+++ frontend/src/app/carrito-compra/carrito-compra.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carrito-compra',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarritoCompraComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+  }
+
+  form(){
+    this.route.navigateByUrl("/pago-formulario");
+  }
+
+  prod(){
+    this.route.navigateByUrl("/productos-catalogo");
   }
 
 }
