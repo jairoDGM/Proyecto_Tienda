312c1bd5854b83dc53dffdb56b5e005f8f2ca86d
jairoDGM <jairo.danielgm@gmail.com>
2022-09-27 21:12:01 -0600

    modificacion db v2

--- db/script_DB.sql
+++ db/script_DB.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE db_tienda_ct;
 USE db_tienda_ct;
 
+--tablas de las entidades
 	CREATE TABLE clientes(
-		id_cliente integer NOT NULL,
+		id_cliente integer NOT NULL AUTO_INCREMENT,
 		nombre char(50),
 		fecha date,
 		contrasena char(50),
@@ -10,36 +11,53 @@ USE db_tienda_ct;
 		PRIMARY KEY (id_cliente)
 	);
 
+	CREATE TABLE rol(
+		tipo_usuario char(10),
+		PRIMARY KEY (tipo_usuario)
+	);
+
 	CREATE TABLE productos(
-		codigo_producto integer NOT NULL,
-		tipo_producto char(50),
+		codigo_producto integer NOT NULL AUTO_INCREMENT,
 		precio float,
 		nombre_producto char(50),
 		cantidad_bodega integer,
+		descripcion_producto char(200),
+		imagen char(255),
 		PRIMARY KEY (codigo_producto)
 	);
 
-CREATE TABLE compra(
-    id_compra integer,
-    nombre_courrier char(50),
-    compania_tarjeta char(50),
-    monto_pagar float,
-    id_cliente integer NOT NULL,
-    codigo_producto integer NOT NULL,
-    PRIMARY KEY (id_compra),
-		FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
-		FOREIGN KEY (codigo_producto) REFERENCES productos(codigo_producto)
-);
+	CREATE TABLE compra(
+		id_compra integer NOT NULL AUTO_INCREMENT,
+		nombre_courrier char(50),
+		compania_tarjeta char(50),
+		estado integer,
+		total_pagar float,
+		PRIMARY KEY (id_compra)
+	);
 
 -- Tablas de las relaciones entre entidades
-CREATE TABLE compra_realizada(
-    id_cliente integer,
-    codigo_producto integer,
-    id_compra integer,
-	monto_pagar float,
-	cantidad_comprada integer,
-    PRIMARY KEY (id_cliente,codigo_producto,id_compra),
-    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
-    FOREIGN KEY (codigo_producto) REFERENCES productos(codigo_producto),
-    FOREIGN KEY (id_compra) REFERENCES compra(id_compra)
+CREATE TABLE asignacion_rol(
+    tipo_usuario char(10),
+	id_cliente integer,
+	PRIMARY KEY (tipo_usuario,id_cliente),
+	FOREIGN KEY (tipo_usuario) REFERENCES rol(tipo_usuario),
+	FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
+);
+
+CREATE TABLE compra_cliente(
+	id_cliente integer,
+	id_compra integer,
+	PRIMARY KEY (id_cliente,id_compra),
+	FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
+	FOREIGN KEY (id_compra) REFERENCES compra(id_compra)
+);
+
+CREATE TABLE carrito(
+	id_cliente integer,
+	codigo_producto integer,
+	cantidad_producto integer,
+	fecha_compra date,
+	PRIMARY KEY (id_cliente,codigo_producto),
+	FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
+	FOREIGN KEY (codigo_producto) REFERENCES productos(codigo_producto)
 );
